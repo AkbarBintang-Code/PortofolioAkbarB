d5cce487190ae79dec9f10820c2c26f6c6be5df4
AkbarBintang-Code <akbarbintang881@gmail.com>
2025-05-14 21:38:51 +0700

    reparasi besar

--- script/main.js
+++ script/main.js
@@ -51,7 +51,6 @@ const navSlide = () => {
     distance: '80px',
     duration: 2500,
     delay: 200,
-    reset: true,
   });
 
   sr.reveal('.main-container .hero-left', { origin: 'left' });
@@ -101,3 +100,25 @@ const navSlide = () => {
   // // Ulangi animasi tiap 2 detik
   // setInterval(animateBlobs, 2000);
 
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('#navbar li a');
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === '#' + current) {
+        link.classList.add('active');
+      }
+    });
+  });
